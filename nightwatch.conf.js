6816071ad30433ac43e376b3f11a11e955dfb818
123Wajid <89453244+123Wajid@users.noreply.github.com>
2022-11-30 23:31:05 +0500

    Update nightwatch.conf.js

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -15,6 +15,7 @@ module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
   src_folders: ['tests'],
+  output_folder: ['tests_output/nightwatch-html-report'],
 
 
   // See https://nightwatchjs.org/guide/concepts/page-object-model.html
